c77466e2c28b56bc1ca53fe8886b9569ce6a2456
femi <oluwafemi.akinyemi@datamellon.com>
2023-05-12 12:18:41 +0100

    Second implementation

--- app/page.js
+++ app/page.js
@@ -1,52 +1,37 @@
 "use client";
 
-import Image from "next/image";
-import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
-// import { Document, Page, Text, PDFViewer, PDFDownloadLink } from 'react-pdf';
-
-import {
-  Document,
-  Page,
-  Text,
-  View,
-  StyleSheet,
-  PDFViewer,
-} from "@react-pdf/renderer";
-
-const styles = StyleSheet.create({
-	page: {
-		flexDirection: 'row',
-	},
-	section: {
-		flexGrow: 1,
-	},
-});
-
+import { saveAs } from "file-saver";
+import { PDFDocument, Page, Text, StandardFonts, rgb } from "pdf-lib";
+import { Configuration, OpenAIApi } from "openai";
 export default function Home() {
-  const [text, setText] = useState("");
-  const [coverLetter, setcoverLetter] = useState();
   const [loading, setLoading] = useState(false);
 
   const [name, setName] = useState("");
   const [company, setCompany] = useState("");
   const [degree, setDegree] = useState("");
+  const [position, setPosition] = useState("");
   const [experience, setExperience] = useState("");
   const [specialtyOne, setSpecialtyOne] = useState("");
   const [specialtyTwo, setSpecialtyTwo] = useState("");
-
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [coverresult, setCoverResult] = useState();
 
   const configuration = new Configuration({
-    // apiKey: process.env.OPENAI_API_KEY,
     organization: "org-rXCK3Wb8ReJytvT5BtWXXpI3",
-    apiKey: "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",
+    apiKey: "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",
   });
+
   const openai = new OpenAIApi(configuration);
 
-  const generateCoverLetter = async (name, position, company) => {
-    const prompt = `Please generate the body of a cover letter for a frontend position at Datamellon Limited, Nigeria. I have a degree in Mathematics with 2 years of experience(s) with a specialty in JavaScript and ReactJs. Make it a maximum of three paragraphs.`;
+  const generateCoverLetter = async (
+    position,
+    company,
+    degree,
+    experience,
+    specialty1,
+    specialty2
+  ) => {
+    const prompt = `Please generate the body of a cover letter for a ${position} position at ${company}, Nigeria. I have a degree in ${degree} with ${experience} years of experience(s) with a specialty in ${specialty1} and ${specialty2}. Make it a maximum of three paragraphs. Add ${name} as the name after the Remarks.`;
 
     openai
       .createCompletion({
@@ -55,11 +40,39 @@ export default function Home() {
         temperature: 0.6,
         max_tokens: 1000,
       })
-      .then((res) => {
+      .then(async (res) => {
         if (res.status === 200) {
           setLoading(false);
-          // setsummarizedtext(res?.data?.choices[0]?.text);
+          console.log(res, "res");
+          setCoverResult(res?.data?.choices[0]?.text);
           console.log(res?.data?.choices[0]?.text);
+          if (res.status === 200) {
+            const pdfDoc = await PDFDocument.create();
+            const timesRomanFont = await pdfDoc.embedFont(
+              StandardFonts.TimesRoman
+            );
+            const page = pdfDoc.addPage([595.28, 841.89]);
+            const { width, height } = page.getSize();
+            const fontSize = 10;
+            const margin = 50;
+            const lineHeight = timesRomanFont.heightAtSize(fontSize) + 2;
+            const lines = res?.data?.choices[0]?.text.split("\n");
+
+            let y = height - margin;
+
+            for (const line of lines) {
+              page.drawText(line, {
+                x: margin,
+                y: y,
+                size: fontSize,
+                font: timesRomanFont,
+                color: rgb(0, 0.53, 0.71),
+              });
+              y -= lineHeight;
+            }
+            const pdfBytes = await pdfDoc.save();
+            saveAs(new Blob([pdfBytes.buffer]), "example.pdf");
+          }
         }
       })
       .catch((err) => {
@@ -67,59 +80,19 @@ export default function Home() {
       });
   };
 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const name = e.target.name.value;
-    const email = e.target.email.value;
-    const position = e.target.position.value;
-    const company = e.target.company.value;
-
-    console.log(
-      "name",
-      name,
-      email,
-      "email",
+    
+    generateCoverLetter(
       position,
-      "position",
       company,
-      "company"
+      degree,
+      experience,
+      specialtyOne,
+      specialtyTwo
     );
-
-    generateCoverLetter(position, company);
   };
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
-
-  const CoverLetter = () => {
-    const letterContent = (
-      <div>
-        <p>Dear Hiring Manager,</p>
-        <p>I am excited to apply for the Frontend position at Datamellon Limited, Nigeria. With a degree in Mathematics and 2 years of experience in JavaScript and ReactJs, I am confident that my skills and experience make me a strong candidate for this role.</p>
-        <p>...</p>
-      </div>
-    );
-  }
-
-
-  // Create Document Component
-  function BasicDocument() {
-    return (
-      <Document>
-		<Page size="A4" style={styles.page}>
-			<View style={styles.section}>
-				<Text>Hello World!</Text>
-			</View>
-			<View style={styles.section}>
-				<Text>We're inside a PDF!</Text>
-			</View>
-		</Page>
-	</Document>
-    );
-  }
-  
 
 
   return (
@@ -156,6 +129,22 @@ export default function Home() {
                 placeholder="Enter company name"
                 value={company}
                 onChange={(e) => setCompany(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block mb-2 font-bold text-gray-700"
+                htmlFor="degree"
+              >
+                Position
+              </label>
+              <input
+                className="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                type="text"
+                placeholder="Enter degree. E.g Frontend developer"
+                value={position}
+                onChange={(e) => setPosition(e.target.value)}
               />
             </div>
 
@@ -231,19 +220,9 @@ export default function Home() {
             </button>
           </form>
 
-
-
-
-<div>
-     
-
-<BasicDocument />
-    </div>
-
-
+          <div></div>
         </div>
       </div>
-   
     </main>
   );
 }
