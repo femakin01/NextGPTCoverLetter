622e0bd77fa6104e5529ea73863c26bc23997649
femi <oluwafemi.akinyemi@datamellon.com>
2023-05-12 15:44:02 +0100

    progress

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
   const [specialtyTwo, setSpecialtyTwo] = useState("");
   const configuration = new Configuration({
     organization: "org-rXCK3Wb8ReJytvT5BtWXXpI3",
-    apiKey: "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",
+    apiKey: "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",
   });
 
   const openai = new OpenAIApi(configuration);
@@ -27,8 +27,11 @@ export default function Home() {
     specialty1,
     specialty2
   ) => {
-    setLoading(true)
-    const prompt = `Please generate the body of a cover letter for a ${position} position at ${company}, Nigeria. I have a degree in ${degree} with ${experience} years of experience(s) with a specialty in ${specialty1} and ${specialty2}. Make it a maximum of three paragraphs. Make the words maximum of twenty words per line  Add ${name} as the name after the Remarks`;
+    setLoading(true);
+    const prompt = `Please generate the body of a cover letter for a ${position} position at ${company}.
+     I have a degree in ${degree} with ${experience} years of experience(s) with a specialty in ${specialty1} and ${specialty2}. 
+     Make it a maximum of three paragraphs. Make the words maximum of twenty words per line  
+     Add ${name} as the name after the Remarks`;
 
     openai
       .createCompletion({
@@ -39,52 +42,58 @@ export default function Home() {
       })
       .then(async (res) => {
         if (res.status === 200) {
+          setName("")
+          setCompany("");
+          setDegree('');
+          setExperience('');
+          setPosition("");
+          setSpecialtyOne('');
+          setSpecialtyTwo('')
           setLoading(false);
           console.log(res, "res");
           console.log(res?.data?.choices[0]?.text);
           if (res.status === 200) {
-    const pdfDoc = await PDFDocument.create();
-    const timesRomanFont = await pdfDoc.embedFont(
-      StandardFonts.TimesRoman
-    );
-    const page = pdfDoc.addPage([595.28, 841.89]);
+            const pdfDoc = await PDFDocument.create();
+            const timesRomanFont = await pdfDoc.embedFont(
+              StandardFonts.TimesRoman
+            );
+            const page = pdfDoc.addPage([595.28, 841.89]);
 
-    const { width, height } = page.getSize();
-    const fontSize = 10;
-    const margin = 50;
-    let y = height - margin;
-    const words = res?.data?.choices[0]?.text.split(' ');
-    const lines = [];
-    let line = '';
-    
-    for (const word of words) {
-      if ((line + word).length > 100) {
-        lines.push(line);
-        line = '';
-      }
-    
-      line += `${word} `;
-    }
-    
-    if (line.length > 0) {
-      lines.push(line);
-    }
-    
+            const { width, height } = page.getSize();
+            const fontSize = 10;
+            const margin = 50;
+            let y = height - margin;
+            const words = res?.data?.choices[0]?.text.split(" ");
+            const lines = [];
+            let line = "";
 
+            for (const word of words) {
+              if ((line + word).length > 100) {
+                lines.push(line);
+                line = "";
+              }
 
-    page.drawText(lines.join('\n'), {
-       x: 50,
-      y: height - 4 * fontSize,
-      size: fontSize,
-      font: timesRomanFont,
-      color: rgb(0, 0.53, 0.71),
-    })
-    const pdfBytes = await pdfDoc.save();
-    saveAs(new Blob([pdfBytes.buffer]), "My_cover_letter.pdf");
+              line += `${word} `;
+            }
+
+            if (line.length > 0) {
+              lines.push(line);
+            }
+
+            page.drawText(lines.join("\n"), {
+              x: 50,
+              y: height - 4 * fontSize,
+              size: fontSize,
+              font: timesRomanFont,
+              color: rgb(0, 0.53, 0.71),
+            });
+            const pdfBytes = await pdfDoc.save();
+            saveAs(new Blob([pdfBytes.buffer]), "My_cover_letter.pdf");
           }
         }
       })
       .catch((err) => {
+        setLoading(false);
         console.log(err, "An error occured");
       });
   };
@@ -105,8 +114,10 @@ export default function Home() {
   return (
     <main className="">
       <div className="flex flex-col items-center justify-center">
-  <h1 className="text-2xl sm:text-2xl md:text-3xl sm:text-2xl font-bold text-center">Cover Letter Generator</h1>
-</div>
+        <h1 className="text-2xl sm:text-2xl md:text-3xl sm:text-2xl font-bold text-center">
+          Cover Letter Generator
+        </h1>
+      </div>
 
       <div className="flex flex-col items-center justify-center h-screen">
         <div className="w-3/4 md:w-1/2">
@@ -234,7 +245,7 @@ export default function Home() {
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
-              {loading ? 'loading...' : 'Generate Cover Letter'}
+              {loading ? "loading..." : "Generate Cover Letter"}
             </button>
           </form>
         </div>
